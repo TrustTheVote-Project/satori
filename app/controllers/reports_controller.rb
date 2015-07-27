c7da15e79df45127f43856b2fcb29daabc392416
Aleksey Gureiev <spyromus@noizeramp.com>
2015-07-27 12:27:47 +0300

    ordering localities in CSV

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < BaseController
   before_action :load_election
 
   def events_by_county
-    @report = EventsByCountyReport.new(@election.records.select("jurisdiction, action, form, count(*) as cnt").group("jurisdiction, action, form"))
+    @report = EventsByCountyReport.new(@election.records.select("jurisdiction, action, form, count(*) as cnt").group("jurisdiction, action, form").order(:jurisdiction))
 
     respond_to do |format|
       format.html
